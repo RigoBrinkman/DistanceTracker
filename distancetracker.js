68db02e928245a2978271c0ed1622276be1d3d8b
Rigo Brinkman <rigobrinkman@gmail.com>
2018-04-21 23:33:08 +0200

    Localstorage

--- distancetracker.js
+++ distancetracker.js
@@ -19,11 +19,22 @@ $.ajax({
 
 
 function outputTable(destination, data){
+    localStorage.setItem("input", document.getElementById("dest-zip").value);
+    var output;
+    
+    input = document.getElementById("file-select");
+    if(!input.files[0]){
+        output = "nee";
+    }else{
+        output = "ja";
+    }
+    var arr = ["een", "twee"];
+    localStorage.setItem("testitm", JSON.stringify(arr));
 	var table = document.getElementById(destination);
 	var row = table.insertRow(0);
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
-	cell1.innerHTML =  "AAA";
-	cell2.innerHTML =  "VVV";
+	cell1.innerHTML =  output;
+    cell2.innerHTML =  localStorage.getItem("input");
 
 }
